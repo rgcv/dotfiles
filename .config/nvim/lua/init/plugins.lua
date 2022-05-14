597bd9fe9b25ed4d5da41518eed2f20703dbc3e0
Rui Ventura <rventura.pt@outlook.com>
2022-05-14 17:38:38 +0100

    nvim: remove fn alias

--- .config/nvim/lua/init/plugins.lua
+++ .config/nvim/lua/init/plugins.lua
@@ -1,7 +1,6 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system({
     'git', 'clone',
     '--depth', '1',
     'https://github.com/wbthomason/packer.nvim',
