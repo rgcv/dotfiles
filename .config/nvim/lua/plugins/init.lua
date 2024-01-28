044a9a93cd56f3f4ee71f3369fe69537092cf0c2
Rui Ventura <rventura.pt@outlook.com>
2024-01-28 18:11:26 +0000

    nvim: replace vim-commentary with Comment.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,6 +33,26 @@ return {
     opts = {},
   },
 
+  -- comment
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      enable_autocmd = false,
+    },
+    config = function(_, opts)
+      require("ts_context_commentstring").setup(opts)
+    end
+  },
+
   -- surround
   {
     "kylechui/nvim-surround",
--- .config/nvim/lua/plugins/tpope.lua
+++ .config/nvim/lua/plugins/tpope.lua
@@ -2,8 +2,6 @@ return {
   "tpope/vim-apathy",
   -- Modern database inferface for Vim
   "tpope/vim-dadbod",
-  -- comment stuff out
-  "tpope/vim-commentary",
   -- A Git wrapper so awesome, it should be illegal
   "tpope/vim-fugitive",
   -- enable repeating supported plugin maps with
