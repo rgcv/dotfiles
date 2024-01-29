20bbd991839ff0f67e77e510887cc289ee3a2ea7
Rui Ventura <rventura.pt@outlook.com>
2024-01-29 11:37:08 +0000

    nvim: add rhubarb (surprised it wasn't there)

--- .config/nvim/lua/plugins/tpope.lua
+++ .config/nvim/lua/plugins/tpope.lua
@@ -4,6 +4,8 @@ return {
   "tpope/vim-dadbod",
   -- A Git wrapper so awesome, it should be illegal
   "tpope/vim-fugitive",
+  -- Fugitive github integration
+  "tpope/vim-rhubarb",
   -- enable repeating supported plugin maps with
   "tpope/vim-repeat",
   -- Heuristically set buffer options
