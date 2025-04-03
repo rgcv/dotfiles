73eecc0a791b1af3d89929abc31ebd8d28475f9e
Rui Ventura <rventura.pt@outlook.com>
2025-04-03 09:21:05 +0100

    nvim: revise lspconfig

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -101,14 +101,45 @@ return {
 
           lua_ls = function()
             require('lspconfig').lua_ls.setup({
-              diagnostics = {
-                globals = { 'vim' },
+              settings = {
+                Lua = {
+                  telemetry = {
+                    enable = false
+                  }
+                }
               },
-              workspace = {
-                library = {
-                  vim.env.VIMRUNTIME,
-                },
-              },
+              on_init = function(client)
+                local join = vim.fs.joinpath
+                local path = client.workspace_folders[1].name
+
+                if vim.uv.fs_stat(join(path, '.luarc.json'))
+                  or vim.uv.fs_stat(join(path, '.luarc.jsonc'))
+                then
+                  return
+                end
+
+                local nvim_settings = {
+                  runtime = {
+                    version = 'LuaJIT'
+                  },
+                  diagnostics = {
+                    globals = {'vim'}
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    library = {
+                      vim.env.RUNTIME,
+                      vim.fn.stdpath('config')
+                    }
+                  },
+                }
+
+                client.config.settings.Lua = vim.tbl_deep_extend(
+                  'force',
+                  client.config.settings.Lua,
+                  nvim_settings
+                )
+              end,
             })
           end,
         }
